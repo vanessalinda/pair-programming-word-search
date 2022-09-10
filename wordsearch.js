1c49b016a1dafcafc052aa83df705a1b18bdf5fa
Vanessa Power <vanessa.l.power@gmail.com>
2022-09-10 10:12:34 -0700

    updating the function and tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
 
-module.exports = wordSearch
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let array = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    array.push(letters.map((array) => array[i]));
+  }
+
+  const verticalJoin = array.map((ls) => ls.join(""));
+  //console.log(verticalJoin);
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
